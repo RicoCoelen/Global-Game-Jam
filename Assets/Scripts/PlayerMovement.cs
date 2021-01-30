ed9aeb25e50ab3ed59253760e7fe9729c5670fc2
sem laan <semlaan@dds.nl>
2021-01-30 11:05:49 +0100

    PlayerMovement script now collects input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
+    private Controls controls;
+    private bool leftMousePressedLastFrame = false;
+    private Vector2 mousePositionLastFrame = Vector2.zero;
+    private new Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        controls = new Controls();
+        controls.Enable();
+    }
+
+    private void Update()
+    {
+        bool leftMousePressed = controls.Gameplay.LeftMouse.ReadValue<float>() == 1;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(controls.Gameplay.MousePosition.ReadValue<Vector2>());
+        leftMousePressedLastFrame = leftMousePressed;
+        mousePositionLastFrame = mousePosition;
+    }
 }
