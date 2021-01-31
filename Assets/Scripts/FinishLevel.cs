3c41738503f70a497f076276b801035c482481e9
sem laan <semlaan@dds.nl>
2021-01-31 14:00:21 +0100

    beef6

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -2,23 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CashPickup : MonoBehaviour
+public class FinishLevel : MonoBehaviour
 {
-    [SerializeField] private int cashAmount;
     [SerializeField] private LayerMask playerLayerMask;
-    private CashCount cashCount;
-
-    private void Awake()
-    {
-        cashCount = FindObjectOfType<CashCount>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (1 << collision.gameObject.layer == playerLayerMask.value)
         {
-            cashCount.AddCash(cashAmount);
-            Destroy(gameObject);
+            GetComponent<SceneSwitcher>().switchScene();
         }
     }
 }
