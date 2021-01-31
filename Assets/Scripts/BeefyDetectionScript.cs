0c4f147b2cc10d7a7b0ba7074628bcfa00fa557e
JosephOliai <josepholiai@gmail.com>
2021-01-31 12:58:26 +0100

    Made a beefy script that removes money when hit

---
+++ Assets/Scripts/BeefyDetectionScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BeefyDetectionScript : MonoBehaviour
+{
+    public float distance;
+    public LayerMask layerMask;
+    public int damage;
+    private CashCount cashCounter;
+    public float timer;
+    private float startTime;
+
+    private void Start()
+    {
+        cashCounter = GameObject.FindGameObjectWithTag("CashCounter").GetComponent<CashCount>();
+    }
+
+    void Update()
+    {
+        Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, distance, layerMask);
+
+        if(hit.Length > 0)
+            cashCounter.RemoveCash(damage);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, distance);
+    }
+}
